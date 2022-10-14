334211d0d45628652e2414c73669169a3cace7bd
samvrit <samvrit.goku@gmail.com>
2022-10-13 23:26:23 -0700

    Add air resistance

--- model_3d_init.m
+++ model_3d_init.m
@@ -1,5 +1,8 @@
 l = 1;  % m
 m = 1;  % kg
+rho = 1.24;  % kg/m^3
+As = sqrt(0.001)*sqrt(0.001);
+Cd = 0.45;
 
 x_init = 0;  % m
 y_init = 4;  % m
@@ -7,12 +10,14 @@ z_init = 0;  % m
 theta_init = 5;  % deg
 phi_init = 20;  % deg
 x_dot_init = 0.6;  % m/s
-y_dot_init = -0.5*0;  % m/s
+y_dot_init = -0.5;  % m/s
 z_dot_init = 0;  % m/s
 theta_dot_init = -10;  % deg/s
 phi_dot_init = 0;  % deg/s
 
 A = [zeros(5,5), eye(5); zeros(5,10)];
+A(7, 7) = (1/m)*rho*As*Cd;  % linearized drag force due to air resistance
+
 B = [0 0 0 0 0
      0 0 0 0 0
      0 0 0 0 0
